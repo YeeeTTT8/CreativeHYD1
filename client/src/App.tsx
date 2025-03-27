10bfd99e459b7517f2ae3e0de2a1ce2e5c737e46
YeeeTTT8 <rishitpradeep90@gmail.com>
2025-03-27 11:11:10 +0530

    Fix GitHub Pages deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Switch } from "wouter";
+import { Route, Switch, Router } from "wouter";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
 import Home from "./pages/Home";
@@ -6,12 +6,19 @@ import CustomCursor from "./components/shared/CustomCursor";
 import Preloader from "./components/shared/Preloader";
 import ScrollToTop from "./components/shared/ScrollToTop";
 
-function Router() {
+// Enable hash-based routing
+const base = window.location.pathname.includes("/CreativeHYD1/")
+  ? "/CreativeHYD1"
+  : "/";
+
+function AppRouter() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <Router base={base}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
@@ -21,7 +28,7 @@ function App() {
       <Preloader />
       <CustomCursor />
       <ScrollToTop />
-      <Router />
+      <AppRouter />
       <Toaster />
     </>
   );
